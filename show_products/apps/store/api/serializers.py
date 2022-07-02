f9a39281f1e459261e70fa0c7cb5f854e769e08a
Antonio Mourao <antonio.smsobrinho@gmail.com>
2022-07-02 15:18:14 -0300

    return date_of_sale count in ProductSerializer

--- show_products/apps/store/api/serializers.py
+++ show_products/apps/store/api/serializers.py
@@ -1,7 +1,8 @@
+from django.db.models import Count
 from rest_framework import serializers
 from apps.store.models import Product
 
-class ProductSerializer(serializers.ModelSerializer):
+class (serializers.ModelSerializer):
 
     class Meta:
         model = Product
@@ -9,11 +10,24 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance:Product):
         representation = super().to_representation(instance)
-        representation['product_url'] = instance.image.url
+        representation['product_url'] = representation['image']
         representation['product_url__created_at'] = representation['created_at']
 
         product_sale = instance.productsale_set.all()
 
         representation['c'] = product_sale.count()
+        representation['consult_date'] = list()
+        product_sale_count = product_sale.order_by(
+                                        'date_of_sale'
+                                    ).values(
+                                        'date_of_sale'
+                                    ).annotate(dcount=Count('date_of_sale'))
 
+        for count in product_sale_count:
+            representation['consult_date'].append(
+                {
+                    'date_of_sale': count['date_of_sale'].strftime('%d/%m/%Y'),
+                    'count': count['dcount']
+                }
+            )
         return representation
