22f80a74ab82bc740af7b5ede83a9cfc44125fcf
Antonio Mourao <antonio.smsobrinho@gmail.com>
2022-07-02 15:05:07 -0300

    update ProductAdmin

--- show_products/apps/store/admin.py
+++ show_products/apps/store/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from apps.store.models import Product
+from apps.store.models import Product, ProductSale
 
 # Register your models here.
 
-admin.site.register(Product)
+class ProductSaleAdmin(admin.TabularInline):
+
+    model = ProductSale
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    inlines = (ProductSaleAdmin, )
