7a492649271f67459c03b1fed08e5d1dc53f6d35
Antonio Mourao <antonio.smsobrinho@gmail.com>
2022-07-02 23:48:57 -0300

    add ProductViewSet ordering

--- show_products/apps/store/api/viewsets.py
+++ show_products/apps/store/api/viewsets.py
@@ -1,5 +1,4 @@
-import imp
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from apps.store.models import Product
 from apps.store.api.serializers import ProductSerializer
 
@@ -7,3 +6,6 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = '__all__'
+    ordering = ['created_at']
